4e1e26b9a54de43d3be99099caadafb2374f83df
JamesLabrosse <james@labrossemusic.com>
2026-05-28 10:15:20 -0400

    Update Tally form height

--- app/masterclass/page.tsx
+++ app/masterclass/page.tsx
@@ -131,7 +131,7 @@ export default function MasterclassPage() {
               data-tally-src="https://tally.so/embed/68pb9o?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
               loading="lazy"
               width="100%"
-              height="1680"
+              height="1365"
               frameBorder={0}
               marginHeight={0}
               marginWidth={0}
